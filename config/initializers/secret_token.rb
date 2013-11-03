fe2a092f750a2ec7efd8baba93b610ee61d7d547
John Richardson <richardsonjm@gmail.com>
2013-11-02 22:31:50 -0400

    adding the rails files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PollywogRanchRailsRuby003::Application.config.secret_key_base = 'dca097dc23e91cab0c6dca195040804167ce719ad1e20500af9ac8c328cbfe55e6376e113e649c752a06fd5daff526b4d3120b94119c5789444ede4cfec87fc4'
