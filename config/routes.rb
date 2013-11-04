9bec39de39d8507360fe53de98259f87977f6ede
John Richardson <richardsonjm@gmail.com>
2013-11-04 10:54:11 -0500

    added index page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ PollywogRanchRailsRuby003::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
+  get '/' => 'site#index'
   resources :ponds, except: [:update]
   post 'ponds/:id/update' => 'ponds#update'
   get 'ponds/:id/destroy' => 'ponds#destroy'
